f383a3c774c3339cf57f96bd6e46d9f92e733772
EagleMoor <eaglemoor@webspirit.pro>
2021-01-20 13:52:30 +0300

    add web

--- crown2/index.js
+++ crown2/index.js
@@ -1,20 +1,20 @@
 var app = new Vue({
-    el: '#app',
-    data() {
-        return {
-        text: '',
-        items: []
-        }
-    },
-    mounted() {
-        let self = this;
-        axios.get("//avalonbot.teso.world/crown/item/")
-          .then(response => {
-            self.items = response.data
-            console.log(self.items);
-          })
-          .catch(error => {
-            console.log(error);
-          });
+  el: '#cardlist',
+  data() {
+    return {
+      text: '',
+      items: []
     }
+  },
+  mounted() {
+    let self = this;
+    axios.get("https://avalonbot.teso.world/crown/item/")
+      .then(response => {
+        self.items = response.data
+        console.log(self.items);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
 })
